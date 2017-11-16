2bf98a9fe382949ede59e49e2af6945fec76eef3
jefflee168 <1919081933@qq.com>
2017-11-16 17:19:42 +0800

    add pexpect and smtplib

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 本python目录主要用于存放自己编写的一些python脚本
 
-1、
+1、pexpect为pexpect类脚本，包括pxssh远程连接；
+
+2、smtplib类为python邮件类，利用这个类可以发送警告或者通知类消息给管理员，使管理能及时了解系统动态。
---
+++ smtplib/send_email.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+# 第三方 SMTP 服务
+mail_host = "smtp.exmail.qq.com"
+mail_user = "test@qq.com"
+mail_pass = "123456"
+
+# 发送邮件邮箱
+sender = 'test@qq.com'
+# 此处填写接收邮件的邮箱，可以是QQ邮件或者其它邮件
+receiver = ['888888@qq.com']
+
+# 邮件发送内容
+message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
+message['From'] = Header("Python 教程", 'utf-8')
+message['To'] =  Header("测试", 'utf-8')
+
+
+# 邮件标题
+title = "Python SMTP 邮件发送测试"
+message['Subject'] = Header(title,'utf-8')
+
+try:
+    server = smtplib.SMTP()
+    server.connect(mail_host,25)
+    server.login(mail_user,mail_pass)
+    server.sendmail(sender,receiver,message.as_string())
+    print "恭喜你，邮件发送成功！"
+except smtplib.SMTPException as e:
+    print "Error: 邮件无法发送..."
+    print str(e)

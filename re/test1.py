1f5c9c6017cc10bbfcd69fe9c55688429a5aab08
jefflee168 <816116193@qq.com>
2018-12-03 21:38:52 +0800

    change file re/test1.py

--- re/test1.py
+++ re/test1.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 
 
+##############################
+# 此脚本实现的功能为：获取页面 
+# http://auto.sina.com.cn/ 
+# 中的所有超链接
+##############################
+
 import re,requests
 
 r = requests.get('http://auto.sina.com.cn/')

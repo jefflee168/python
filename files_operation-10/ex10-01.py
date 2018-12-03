891d80924ac0d632d0d9cce02d6ee4525ebc726a
jefflee168 <816116193@qq.com>
2018-12-03 22:05:56 +0800

    change re/ex10-01.py

--- files_operation-10/ex10-01.py
+++ files_operation-10/ex10-01.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 _8-
 
+# 本例使用上下文管理器，使代码更加简洁优美
+
 try:
-    f = open('/home/jeff/1.txt')
-    print(f.read())
+#    f = open('/home/jeff/1.txt')
+#    print(f.read())
+    with open('/home/jeff/1.txt') as f:
+        print(f.read())
 finally:
     f.close()
